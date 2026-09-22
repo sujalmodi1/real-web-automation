16ec357a1d0c939eda4b4f3b4c3cc7dc9c13346d
Sujal Modi <sujalmodi46@gmail.com>
2026-09-23 05:24:24 +0530

    Refactor isProductsPageDisplayed method for clarity

--- src/main/java/pages/ProductsPage.java
+++ src/main/java/pages/ProductsPage.java
@@ -28,7 +28,7 @@ public class ProductsPage {
     }
 
     public boolean isProductsPageDisplayed() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(pageTitle)).getText().equals("Products");
+        return wait.until(ExpectedConditions.textToBeDisplayedInElementLocated(pageTitle, "Products"));
     }
 
     public void sortBy(String value) {
